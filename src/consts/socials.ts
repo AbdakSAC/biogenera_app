b046f6ed17337d530a36a3a2009309df8fd48cc6
Azazek6 <naturaespinola12@gmail.com>
2025-04-05 10:07:48 -0500

    change: contact phone

---
+++ src/consts/socials.ts
@@ -0,0 +1,16 @@
+export const SOCIALS = [
+  {
+    title: "Conoce más sobre",
+    title_special: "Brenner Vera.",
+    description: "Conoce sobre Brenner, su trayectoria, logros y proyectos.",
+    url: "/Img/1.webp ",
+    link: "https://brennervera.com/",
+  },
+  {
+    title: "Comunícate con un",
+    title_special: "asesor especializado.",
+    description: "Resuelve tus dudas y preguntas con la ayuda de un asesor.",
+    url: "/Img/2.webp",
+    link: "https://wa.link/y9csr2",
+  },
+];
